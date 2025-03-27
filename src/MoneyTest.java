959f328e48388924df8e648a51f99411114d54a7
Maria-Emilia1 <mariaemiliay99@gmail.com>
2025-03-27 05:49:37 -0300

    update nas importações

--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -1,3 +1,8 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
 public class MoneyTest {
     @Test
     public void testCurrency() {
