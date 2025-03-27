f24404621f7ad0ef7421a652e8ec27364100c511
Maria-Emilia1 <mariaemiliay99@gmail.com>
2025-03-27 05:40:17 -0300

    Dollar imutável, times() retorna novo objeto

--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -5,7 +5,7 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 }
