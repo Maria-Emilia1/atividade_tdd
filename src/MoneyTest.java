a5a69e3fb87aa75013e0e3058f768cdd2f0542bd
Maria-Emilia1 <mariaemiliay99@gmail.com>
2025-03-27 05:41:14 -0300

    Método equals() e amount como private

--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -3,9 +3,8 @@ import static org.junit.Assert.*;
 
 public class MoneyTest {
     @Test
-    public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
